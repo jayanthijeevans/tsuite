80535bf03b662442cd73bd8fca2e892e98948faa
jayanthi <jayanthi.jeevans@gmail.com>
2022-09-15 12:57:05 +0530

    first commit

---
+++ src/test/java/testcases/NewTest1.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class NewTest1 {
+  @Test
+  public void f() {
+	  System.out.println("fgkfdj");
+	  Assert.assertEquals("originalTitle", "expectedTitle", "Titles of the website do not match");
+  }
+}
---
+++ src/test/java/testcases/NewTest2.java
@@ -0,0 +1,11 @@
+package testcases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+  @Test
+  public void f() {
+	  System.out.println("nbnxcbv");
+  }
+}
